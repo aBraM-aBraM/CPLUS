73e689b1e3d12d38ffe7eb5721c695b500b39c61
aBram <eyalandron@gmail.com>
2022-12-16 15:39:03 +0200

    mocks: comment example

--- examples/mocks.cpp
+++ examples/mocks.cpp
@@ -1,3 +1,7 @@
+/*
+* This is my implementation of mocking
+* using trait interfaces, uncoupled to any standard library
+*/
 #include <cstdio>
 #include "include/macros.h"
 #include "include/mocks.h"
@@ -5,7 +9,6 @@
 #include <etl/type_traits.h>
 #include <etl/optional.h>
 
-// wip
 
 template<typename T>
 class ICompressable
@@ -37,16 +40,21 @@ int hook_compress2()
 int main()
 {
     MockCompress mock;
+    
     MockMethod(mock, compress).set_return(4);
-    MockMethod(mock, compress).hook(&MockCompress::hook_compress);
-    printf("ret val: %d\n" , mock.compress());
-    printf("call count: %d\n\n", MockMethod(mock, compress).get_call_count());
 
+    // return value is the last set return value
     MockMethod(mock, compress).unhook();
     printf("ret val: %d\n" , mock.compress());
     printf("call count: %d\n\n", MockMethod(mock, compress).get_call_count());
 
+    // return value is the value returned by the hooking function
     MockMethod(mock, compress).hook(&hook_compress2);
+    printf("ret val: %d\n" , mock.compress());
+    printf("call count: %d\n\n", MockMethod(mock, compress).get_call_count());
+
+    // hook with member function
+    MockMethod(mock, compress).hook(&MockCompress::hook_compress);
     printf("ret val: %d\n" , mock.compress());
     printf("call count: %d\n", MockMethod(mock, compress).get_call_count());
 
