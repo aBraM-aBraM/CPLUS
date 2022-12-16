36791dffb4231724e2484e5af8b08bf649799f68
aBram <eyalandron@gmail.com>
2022-12-16 16:41:02 +0200

    mocks: support const funcs

--- examples/mocks.cpp
+++ examples/mocks.cpp
@@ -1,33 +1,43 @@
 /*
-* This is my implementation of mocking
-* using trait interfaces, uncoupled to any standard library
-*/
+ * This is my implementation of mocking
+ * using trait interfaces, uncoupled to any standard library
+ */
 #include <cstdio>
 #include "include/macros.h"
 #include "include/mocks.h"
 
 #include <etl/type_traits.h>
-#include <etl/optional.h>
+#include <etl/unordered_map.h>
+#include <etl/string.h>
 
-
-template<typename T>
+template <typename T>
 class ICompressable
 {
 public:
     INTERFACE(ICompressable)
     {
-        METHOD(int ,compress,());
+        METHOD(int, compress, () const);
+        METHOD(char *, get_str, ());
     }
 };
 
 class MockCompress : public ICompressable<MockCompress>
 {
-    public:
-    MockCompress(): ICompressable(){}
-    int hook_compress() {
-        printf("hooked!\n");return 0;
+public:
+    MockCompress() : ICompressable() {}
+    int hook_compress()
+    {
+        printf("hooked!\n");
+        return 0;
     }
-    CREATE_MOCK_METHOD(int, compress,());
+    static const int INSTANCE_COUNT = 2;
+    // creates a mock for the given method
+    CREATE_MOCK_METHOD(int, compress, () const, INSTANCE_COUNT);
+    CREATE_MOCK_METHOD(char*, get_str, (), INSTANCE_COUNT);
+
+    // behind the scenes CREATE_MOCK_METHOD creates a static unordered_map for
+    // meta data. The size of this map can be evaluated in the following manner:
+    const size_t size = MockCompress::compressFunc::_get_map_size();
 };
 
 int hook_compress2()
@@ -36,27 +46,46 @@ int hook_compress2()
     return 0;
 }
 
-
 int main()
 {
-    MockCompress mock;
-    
-    MockMethod(mock, compress).set_return(4);
+    MockCompress mock1;
+
+    MockMethod(mock1, compress).set_return(4);
 
     // return value is the last set return value
-    MockMethod(mock, compress).unhook();
-    printf("ret val: %d\n" , mock.compress());
-    printf("call count: %d\n\n", MockMethod(mock, compress).get_call_count());
+    MockMethod(mock1, compress).unhook();
+    printf("ret val: %d\n", mock1.compress());
+    printf("call count: %d\n\n", MockMethod(mock1, compress).get_call_count());
 
     // return value is the value returned by the hooking function
-    MockMethod(mock, compress).hook(&hook_compress2);
-    printf("ret val: %d\n" , mock.compress());
-    printf("call count: %d\n\n", MockMethod(mock, compress).get_call_count());
+    MockMethod(mock1, compress).hook(&hook_compress2);
+    printf("ret val: %d\n", mock1.compress());
+    printf("call count: %d\n\n", MockMethod(mock1, compress).get_call_count());
 
     // hook with member function
-    MockMethod(mock, compress).hook(&MockCompress::hook_compress);
-    printf("ret val: %d\n" , mock.compress());
-    printf("call count: %d\n", MockMethod(mock, compress).get_call_count());
+    MockMethod(mock1, compress).hook(&MockCompress::hook_compress);
+    printf("ret val: %d\n", mock1.compress());
+    printf("call count: %d\n\n", MockMethod(mock1, compress).get_call_count());
+
+    // different mocks instance store different metadata
+    MockCompress mock2;
+    MockMethod(mock1, compress).unhook();
+    MockMethod(mock1, compress).set_return(1);
+    MockMethod(mock2, compress).set_return(2);
+
+    printf("ret val1: %d\n", mock1.compress());
+    printf("ret val2: %d\n\n", mock2.compress());
+
+    printf("call count1: %d\n", MockMethod(mock1, compress).get_call_count());
+    printf("call count2: %d\n\n", MockMethod(mock2, compress).get_call_count());
+
+    MockMethod(mock1, get_str).set_return("Hello ");
+    MockMethod(mock2, get_str).set_return("World!");
+
+    printf("%s %s\n\n", mock1.get_str(), mock2.get_str());
+
+    printf("call count1: %d\n", MockMethod(mock1, get_str).was_called());
+    printf("call count2: %d\n", MockMethod(mock2, get_str).get_call_count());
 
     return 0;
 }
--- include/mocks.h
+++ include/mocks.h
@@ -1,56 +1,67 @@
+#include <etl/variant.h>
+#include <etl/unordered_map.h>
 
-#define MockMethod(mock, method) mock.method##_struct
+#define MockMethod(mock, method) mock.m_##method
 
-template <typename C, void (C::*M)()>
-void AsFunc(void *p)
+template <typename FuncType, typename ReturnType>
+struct MockMetadata
 {
-    (static_cast<C *>(p)->*M)();
-}
+    FuncType _func_ = nullptr;
+    size_t m_call_count = 0;
+    ReturnType m_ret_val{};
+};
 
-#define CREATE_MOCK_METHOD(lhs, name, rhs)                                         \
-    struct name##StructType                                                        \
-    {                                                                              \
-        static int func(name##StructType *obj)                                     \
-        {                                                                          \
-            obj->m_call_count++;                                                   \
-            if (nullptr != obj->_func_)                                            \
-            {                                                                      \
-                return obj->_func_();                                              \
-            }                                                                      \
-            return obj->m_ret_val;                                                 \
-        }                                                                          \
-        using ReturnType = decltype(((name##StructType *)nullptr)->func(nullptr)); \
-        using FuncType = ReturnType (*) rhs;                                       \
-                                                                                   \
-        template <typename GenericFunc>                                            \
-        void hook(GenericFunc hook_function)                                       \
-        {                                                                          \
-            _func_ = reinterpret_cast<FuncType>(hook_function);                    \
-        }                                                                          \
-        void unhook()                                                              \
-        {                                                                          \
-            _func_ = nullptr;                                                      \
-        }                                                                          \
-        void set_return(ReturnType value)                                          \
-        {                                                                          \
-            m_ret_val = value;                                                     \
-        }                                                                          \
-        size_t get_call_count()                                                    \
-        {                                                                          \
-            return m_call_count;                                                   \
-        }                                                                          \
-        bool was_called()                                                          \
-        {                                                                          \
-            return m_call_count > 0;                                               \
-        }                                                                          \
-                                                                                   \
-    private:                                                                       \
-        FuncType _func_ = nullptr;                                                 \
-        size_t m_call_count = 0;                                                   \
-        ReturnType m_ret_val{};                                                    \
-    } name##_struct;                                                               \
-                                                                                   \
-    lhs name rhs                                                                   \
-    {                                                                              \
-        return this->name##_struct.func(&name##_struct);                           \
+template <size_t INSTANCE_COUNT, typename FuncType, typename ReturnType>
+using InstanceMetaMap = etl::unordered_map<size_t, MockMetadata<FuncType, ReturnType>, INSTANCE_COUNT>;
+
+#define CREATE_MOCK_METHOD(lhs, name, rhs, instance_count)                                         \
+    struct name##Func                                                                              \
+    {                                                                                              \
+        static lhs func(const name##Func *obj)                                                     \
+        {                                                                                          \
+            obj->meta->m_call_count++;                                                             \
+            if (nullptr != obj->meta->_func_)                                                      \
+            {                                                                                      \
+                return obj->meta->_func_();                                                        \
+            }                                                                                      \
+            return obj->meta->m_ret_val;                                                           \
+        }                                                                                          \
+        using ReturnType = decltype(((name##Func *)nullptr)->func(nullptr));                       \
+        using FuncType = ReturnType (*)();                                                         \
+        using MapType = InstanceMetaMap<instance_count, FuncType, ReturnType>;                     \
+        template <typename GenericFunc>                                                            \
+        void hook(GenericFunc hook_function)                                                       \
+        {                                                                                          \
+            meta->_func_ = reinterpret_cast<FuncType>(hook_function);                              \
+        }                                                                                          \
+        void unhook()                                                                              \
+        {                                                                                          \
+            meta->_func_ = nullptr;                                                                \
+        }                                                                                          \
+        void set_return(ReturnType value)                                                          \
+        {                                                                                          \
+            meta->m_ret_val = value;                                                               \
+        }                                                                                          \
+        size_t get_call_count() const                                                              \
+        {                                                                                          \
+            return meta->m_call_count;                                                             \
+        }                                                                                          \
+        bool was_called() const                                                                    \
+        {                                                                                          \
+            return meta->m_call_count > 0;                                                         \
+        }                                                                                          \
+                                                                                                   \
+        static size_t _get_map_size()                                                              \
+        {                                                                                          \
+            return sizeof(name##MetaMap);                                                          \
+        }                                                                                          \
+                                                                                                   \
+    private:                                                                                       \
+        static inline MapType name##MetaMap;                                                       \
+        MockMetadata<FuncType, ReturnType> *meta = &name##MetaMap[reinterpret_cast<size_t>(this)]; \
+    } m_##name;                                                                                    \
+                                                                                                   \
+    lhs name rhs                                                                                   \
+    {                                                                                              \
+        return this->m_##name.func(&m_##name);                                                     \
     }
