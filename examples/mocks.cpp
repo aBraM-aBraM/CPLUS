627e95c2313e95fdbdbcfafee17634d70f4dfd54
aBram <eyalandron@gmail.com>
2022-12-16 14:45:08 +0200

    mocks: add method mocking setup macro

--- examples/mocks.cpp
+++ examples/mocks.cpp
@@ -17,58 +17,20 @@ public:
     }
 };
 
-
-// this should be generated w/macros
 class MockCompress : public ICompressable<MockCompress>
 {
-    
     public:
-    MockCompress(): ICompressable()
-    {
-
+    MockCompress(): ICompressable(){}
+    int hook_compress() {
+        printf("hooked!\n");return 0;
     }
-    int hook_compress()
-    {
-        printf("hooked!\n"); // hooks would be used for mocking
-        return 0;
-    }
-
-    struct CompressStructType {
-        static int func(CompressStructType* obj)
-        {
-            obj->m_call_count++; 
-            if(nullptr != obj->_func_) {
-                return obj->_func_();
-            }
-            return obj->m_ret_val;
-        }
-        using ReturnType = decltype(((CompressStructType*)nullptr)->func(nullptr));
-
-        using FuncType = ReturnType(*)();
-
-        template<typename GenericFunc>
-        void hook(GenericFunc hook_function) {_func_ = reinterpret_cast<FuncType>(hook_function);}
-        void unhook() {_func_ = nullptr;}
-        void set_return(ReturnType value) {m_ret_val = value;}
-        size_t get_call_count() {return m_call_count;}
-        bool was_called() {return m_call_count > 0;}
-
-        private:
-        FuncType _func_ = nullptr;
-        size_t m_call_count = 0;
-        ReturnType m_ret_val {};
-
-    } compress_struct;
-
-    int compress() {
-        return this->compress_struct.func(&compress_struct);
-    }
+    CREATE_MOCK_METHOD(int, compress,());
 };
 
 int hook_compress2()
 {
-        printf("hooked2!\n"); // hooks would be used for mocking
-        return 0;
+    printf("hooked2!\n");
+    return 0;
 }
 
 
--- include/mocks.h
+++ include/mocks.h
@@ -2,7 +2,55 @@
 #define MockMethod(mock, method) mock.method##_struct
 
 template <typename C, void (C::*M)()>
-void AsFunc(void* p)
+void AsFunc(void *p)
 {
-    (static_cast<C*>(p)->*M)();
+    (static_cast<C *>(p)->*M)();
 }
+
+#define CREATE_MOCK_METHOD(lhs, name, rhs)                                         \
+    struct name##StructType                                                        \
+    {                                                                              \
+        static int func(name##StructType *obj)                                     \
+        {                                                                          \
+            obj->m_call_count++;                                                   \
+            if (nullptr != obj->_func_)                                            \
+            {                                                                      \
+                return obj->_func_();                                              \
+            }                                                                      \
+            return obj->m_ret_val;                                                 \
+        }                                                                          \
+        using ReturnType = decltype(((name##StructType *)nullptr)->func(nullptr)); \
+        using FuncType = ReturnType (*) rhs;                                       \
+                                                                                   \
+        template <typename GenericFunc>                                            \
+        void hook(GenericFunc hook_function)                                       \
+        {                                                                          \
+            _func_ = reinterpret_cast<FuncType>(hook_function);                    \
+        }                                                                          \
+        void unhook()                                                              \
+        {                                                                          \
+            _func_ = nullptr;                                                      \
+        }                                                                          \
+        void set_return(ReturnType value)                                          \
+        {                                                                          \
+            m_ret_val = value;                                                     \
+        }                                                                          \
+        size_t get_call_count()                                                    \
+        {                                                                          \
+            return m_call_count;                                                   \
+        }                                                                          \
+        bool was_called()                                                          \
+        {                                                                          \
+            return m_call_count > 0;                                               \
+        }                                                                          \
+                                                                                   \
+    private:                                                                       \
+        FuncType _func_ = nullptr;                                                 \
+        size_t m_call_count = 0;                                                   \
+        ReturnType m_ret_val{};                                                    \
+    } name##_struct;                                                               \
+                                                                                   \
+    lhs name rhs                                                                   \
+    {                                                                              \
+        return this->name##_struct.func(&name##_struct);                           \
+    }
